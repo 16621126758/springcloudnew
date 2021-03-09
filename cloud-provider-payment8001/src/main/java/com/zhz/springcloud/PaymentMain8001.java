f2eecb41b3933317d8b181e88dbfef395265bb1b
赵洪志 <zhao_hongzhi@topsec.com.cn>
2021-03-09 15:23:40 +0800

    创建生产者和消费者

--- cloud-provider-payment8001/src/main/java/com/zhz/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/zhz/springcloud/PaymentMain8001.java
@@ -3,10 +3,12 @@ package com.zhz.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //@MapperScan("com.zhz.springcloud.dao")
 @MapperScan("com.zhz.springcloud.dao")
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
