15685629ac818a40f84a06e71a3f1c83f1969a72
赵洪志 <zhao_hongzhi@topsec.com.cn>
2021-03-15 19:16:44 +0800

    30.订单服务注册金zookeeper

---
+++ cloud-consumerzk-order80/src/main/java/com/zhz/springcloud/controller/OrderZKController.java
@@ -0,0 +1,21 @@
+package com.zhz.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OrderZKController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private static final String INVOKE_URL="http://cloud-provider-payment";
+
+    @GetMapping("/consumer/payment/zk")
+    public String paymentInfo(){
+        String result = restTemplate.getForObject(INVOKE_URL+"/payment/zk",String.class);
+        return result;
+    }
+}
