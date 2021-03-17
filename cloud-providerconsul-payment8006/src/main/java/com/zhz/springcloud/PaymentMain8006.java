efe7c6ed2cf1ede167ed425a9290df32135ad640
赵洪志 <zhao_hongzhi@topsec.com.cn>
2021-03-17 14:07:19 +0800

    33.服务注册者注册进consul

---
+++ cloud-providerconsul-payment8006/src/main/java/com/zhz/springcloud/PaymentMain8006.java
@@ -0,0 +1,15 @@
+package com.zhz.springcloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@Slf4j
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class,args);
+    }
+}
