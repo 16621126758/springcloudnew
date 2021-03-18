6493dd64d5500fd8cb22b56fe5a327127db0c905
赵洪志 <zhao_hongzhi@topsec.com.cn>
2021-03-18 15:14:22 +0800

    37.Ribbon负载均衡和Rest调用

--- cloud-consumer-order80/src/main/java/com/zhz/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/zhz/springcloud/controller/OrderController.java
@@ -1,9 +1,11 @@
 package com.zhz.springcloud.controller;
 
+import cn.hutool.json.JSON;
 import com.zhz.springcloud.entities.CommonResult;
 import com.zhz.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +24,7 @@ public class OrderController {
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment){
         return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
+//        return restTemplate.postForEntity(PAYMENT_URL+"/payment/create",payment,CommonResult.class).getBody();
     }
 
     @GetMapping("consumer/payment/get/{id}")
@@ -29,4 +32,18 @@ public class OrderController {
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
     }
 
-}
+    @GetMapping("consumer/payment/getForEntity/{id}")
+
+    public CommonResult<Payment> getPaymentById2(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+        log.info("the entity is:"+ entity.toString() );
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else{
+            return new CommonResult<>(444,"操作失败");
+        }
+
+    }
+
+
+    }
