a1d2b46bdb773d5245b194a18d7735cc3c643d9a
赵洪志 <zhao_hongzhi@topsec.com.cn>
2021-04-06 18:07:01 +0800

    44.OpenFeign服务调用

---
+++ cloud-consumer-feign-order80/src/main/java/com/zhz/springcloud/OrderFeignMain80.java
@@ -0,0 +1,10 @@
+package com.zhz.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80 {
+}
