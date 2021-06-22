0b1b899d376383c5f28bd4e886e0f87f2268cecb
zhzmby <915032838@qq.com>
2021-06-22 22:42:48 +0800

    56.Hystrix之全局服务降级DeaultProperties(defaultFallback='')

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/zhz/springcloud/controller/OrderHystirxController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/zhz/springcloud/controller/OrderHystirxController.java
@@ -1,5 +1,6 @@
 package com.zhz.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.zhz.springcloud.service.PaymentHystrixService;
@@ -12,6 +13,9 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod",
+        commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",
+                value = "2000")})
 public class OrderHystirxController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
@@ -24,9 +28,10 @@ public class OrderHystirxController {
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "5000")
-    })
+//    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "5000")
+//    })
+    @HystrixCommand
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         return result;
@@ -36,5 +41,9 @@ public class OrderHystirxController {
         return "我是消费者80，对方系统繁忙请10秒后再试，或者自己运行出错，请检查自己，哭了的表情！";
     }
 
+
+    public String payment_Global_FallbackMethod(){
+        return "Global异常处理信息，请稍后再试/(ㄒoㄒ)/~~";
+    }
 
 }
