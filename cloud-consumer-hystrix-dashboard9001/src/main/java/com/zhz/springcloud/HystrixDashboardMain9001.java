3dc3d7ff232ba328430fe5305fe4e51de8739157
zhzmby <915032838@qq.com>
2021-07-08 22:25:47 +0800

    63.Hystrix图形化Dashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/zhz/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,21 @@
+package com.zhz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Class: HystrixDashboardMain9001
+ * @description:
+ * @Author: hongzhi.zhao
+ * @Date: 2021-07-08 22:17
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+
+}
